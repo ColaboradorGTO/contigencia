5ef472c8bb43ed01c0bc29a2ac201ffe08102264
Myltiane Alves <myltiane.aux@gmail.com>
2026-01-19 11:14:14 -0300

    refactor: url validar contigencia

--- src/Informatica/ConsultaNFCE/controllers/statusNfce.js
+++ src/Informatica/ConsultaNFCE/controllers/statusNfce.js
@@ -135,7 +135,7 @@ class ConsultaStatusNfeController {
         if (dataInicio) queryParams.append('dataInicio', dataInicio);
         if (dataFim) queryParams.append('dataFim', dataFim);
         
-        const apiUrl = `http://164.152.245.77:8000/quality/concentrador/api/venda/valida-venda-contingencia.xsjs?page=${page}&pageSize=${pageSize}&dataLimite=${dataInicio}&dataFechamento=${dataFim}`;
+        const apiUrl = `http://164.152.245.77:8000/quality/concentrador/api/venda/valida-venda-contingencia.xsjs?page=${page}&pageSize=${pageSize}&dataInicio=${dataInicio}&dataFim=${dataFim}`;
        
         const response = await axios.get(apiUrl);
         vendas = response.data;
